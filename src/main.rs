cfaadd57cf705f3adaecc9b5f4a541523fee77df
Lukas Orsvarn <lukas@orsvarn.com>
2020-11-26 22:18:57 +0100

    Basic camera test working

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,60 @@
-use bevy::prelude::*;
+use bevy::{
+    prelude::*,
+    transform::components::Transform,
+};
 
-struct Person;
-struct Name(String);
-pub struct HelloPlugin;
-
-impl Plugin for HelloPlugin {
-    fn build(&self, app: &mut AppBuilder) {
-        app.add_resource(GreetTimer(Timer::from_seconds(2.0, true)))
-            .add_startup_system(add_people.system())
-            .add_system(greet_people.system());
-    }
-}
 
 fn main() {
     App::build()
+        .add_resource(WindowDescriptor {
+            title: "Bevy".to_string(),
+            width: 1024,
+            height: 768,
+            vsync: false,
+            ..Default::default()
+        })
         .add_plugins(DefaultPlugins)
-        .add_plugin(HelloPlugin)
+        .add_startup_system(setup.system())
+        // .add_system(toggle_cursor)
+        .add_system(update_camera.system())
         .run();
 }
 
-fn add_people(mut commands: Commands) {
+fn setup(
+    commands: &mut Commands,
+    asset_server: Res<AssetServer>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    let texture_handle = asset_server.load("image_2.png");
+
     commands
-        .spawn((Person, Name("Lukas".to_string())))
-        .spawn((Person, Name("Max".to_string())))
-        .spawn((Person, Name("Joel".to_string())));
+        .spawn(Camera2dBundle::default())
+        .with(Camera)
+        .spawn(SpriteBundle {
+            material: materials.add(texture_handle.into()),
+            ..Default::default()
+        });
 }
 
-struct GreetTimer(Timer);
+struct MyPosition { x: f32, y: f32 }
+struct Camera;
 
-fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<(&Person, &Name)>) {
-    timer.0.tick(time.delta_seconds);
-
-    if timer.0.finished {
-        for (_person, name) in query.iter() {
-            println!("hello {}!", name.0);
-        }
+fn update_camera(mut query: Query<(&mut Transform, &Camera)>) {
+    for (mut transform, my_struct) in query.iter_mut() {
+        transform.translation.x += 1.;
     }
 }
+
+fn toggle_cursor(input: Res<Input<KeyCode>>, mut windows: ResMut<Windows>) {
+    // if input.just_pressed(KeyCode::Space) {
+    //     viewport.locked = !viewport.locked;
+    // }
+
+    // if viewport.locked {
+    //     let window = windows.get_primary_mut().unwrap();
+    //     window.set_cursor_lock_mode(!window.cursor_locked());
+    //     window.set_cursor_visibility(!window.cursor_visible());
+    // }
+}
+
 
