915ce2c026dffd83d7e7265c9ceb6d51723edb21
Lukas Orsvarn <lukas@orsvarn.com>
2021-04-29 20:18:10 +0200

    wip: Can now edit resize policy and filter
    Have to push new version of kanter_core.

--- src/edit_node.rs
+++ src/edit_node.rs
@@ -1,7 +1,7 @@
 use bevy::prelude::*;
 use kanter_core::{
     dag::TextureProcessor,
-    node::{Node, NodeType, ResizeFilter, ResizePolicy},
+    node::{Node, NodeType, ResizeFilter, ResizePolicy, Side},
     node_data::Size as TPSize,
     node_graph::{NodeId, SlotId},
 };
@@ -17,6 +17,7 @@ enum EditState {
     Outer,
     Inner,
     Size,
+    Slot,
 }
 
 #[derive(Clone, Debug)]
@@ -63,16 +64,28 @@ impl Plugin for EditNodePlugin {
                             .with_run_criteria(State::on_enter(EditState::Outer)),
                     )
                     .with_system(
-                        edit_size_enter
+                        edit_specific_size_enter
                             .system()
                             .with_run_criteria(State::on_update(ToolState::EditNode))
                             .with_run_criteria(State::on_enter(EditState::Size)),
                     )
                     .with_system(
-                        edit_size
+                        edit_specific_size_update
                             .system()
                             .with_run_criteria(State::on_update(ToolState::EditNode))
                             .with_run_criteria(State::on_update(EditState::Size)),
+                    )
+                    .with_system(
+                        edit_specific_slot_enter
+                            .system()
+                            .with_run_criteria(State::on_update(ToolState::EditNode))
+                            .with_run_criteria(State::on_enter(EditState::Slot)),
+                    )
+                    .with_system(
+                        edit_specific_slot_update
+                            .system()
+                            .with_run_criteria(State::on_update(ToolState::EditNode))
+                            .with_run_criteria(State::on_update(EditState::Slot)),
                     )
                     .with_system(
                         tool_exit
@@ -121,59 +134,141 @@ fn tool_update(
     }
 }
 
-fn edit_size_enter(
+fn edit_specific_slot_enter(
+    mut edit_state: ResMut<State<EditState>>,
     mut q_instructions: Query<&mut Text, With<InstructionMarker>>,
     q_active: Query<&NodeId, With<Active>>,
     tex_pro: ResMut<TextureProcessor>,
 ) {
     if let (Ok(node_id), Ok(mut instructions)) = (q_active.single(), q_instructions.single_mut()) {
-        if let Some(node_size) = tex_pro.get_node_size(*node_id) {
-            instructions.sections[0].value = format!(
-                "Current size: {}x{}\nNew size: ",
-                node_size.width, node_size.height
-            );
-        } else {
-            instructions.sections[0].value = format!("Example format: 256x256\nNew size: ");
+        if let Some(node) = tex_pro.node_graph.node_with_id(*node_id) {
+            if node.capacity(Side::Input) == 0 {
+                warn!("The node doesn't have any input slots");
+                edit_state.overwrite_set(EditState::Outer).unwrap();
+                return;
+            } else if let ResizePolicy::SpecificSlot(slot) = node.resize_policy {
+                instructions.sections[0].value = format!("Current: {}\nNew: ", slot);
+            } else {
+                instructions.sections[0].value = format!(
+                    "Available IDs are 0 through {}\nChoice: ",
+                    node.capacity(Side::Input)
+                );
+            }
+        }
+        instructions.sections[1].value.clear();
+    }
+}
+
+fn edit_specific_slot_update(
+    mut char_input_events: EventReader<ReceivedCharacter>,
+    mut edit_state: ResMut<State<EditState>>,
+    q_active: Query<&NodeId, With<Active>>,
+    mut tex_pro: ResMut<TextureProcessor>,
+    mut q_instructions: Query<&mut Text, With<InstructionMarker>>,
+    mut started: Local<bool>,
+) {
+    // This guard drops any input the first time the system is entered, so you do not get the
+    // input from the button that was pressed to start this sytem, in this sytem.
+    if !*started {
+        *started = true;
+        return;
+    }
+
+    if let (Ok(mut instructions), Ok(node_id)) = (q_instructions.single_mut(), q_active.single()) {
+        for event in char_input_events.iter() {
+            if is_number(&event.char) {
+                instructions.sections[1].value.push(event.char);
+            } else if event.char == '\u{8}' {
+                // Backspace
+                instructions.sections[1].value.pop();
+            } else if event.char == '\r' {
+                // Enter
+                if let Ok(slot_id) = instructions.sections[1].value.parse::<u32>() {
+                    if let Some(node) = tex_pro.node_graph.node_with_id_mut(*node_id) {
+                        let slot_id = SlotId(slot_id);
+                        if node.slot_exists(slot_id, Side::Input) {
+                            node.resize_policy = ResizePolicy::SpecificSlot(slot_id);
+                        } else {
+                            warn!("Node does not have a slot with the given ID: {}", slot_id);
+                        }
+                    } else {
+                        error!("The node you're trying to edit does not exist: {}", node_id);
+                    }
+                } else {
+                    error!("Could not parse the input as a number: {}", instructions.sections[1].value);
+                }
+                edit_state.overwrite_replace(EditState::Outer).unwrap();
+                *started = false;
+            }
+        }
+    }
+}
+
+fn edit_specific_size_enter(
+    mut q_instructions: Query<&mut Text, With<InstructionMarker>>,
+    q_active: Query<&NodeId, With<Active>>,
+    tex_pro: ResMut<TextureProcessor>,
+) {
+    if let (Ok(node_id), Ok(mut instructions)) = (q_active.single(), q_instructions.single_mut()) {
+        if let Some(node) = tex_pro.node_graph.node_with_id(*node_id) {
+            if let ResizePolicy::SpecificSize(size) = node.resize_policy {
+                instructions.sections[0].value =
+                    format!("Current: {}x{}\nNew: ", size.width, size.height);
+            } else {
+                instructions.sections[0].value = "Example format: 256x256\nNew: ".to_string();
+            }
         }
         instructions.sections[1].value.clear();
     }
 }
 
 fn is_number(character: &char) -> bool {
-    matches!(character, '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' )
+    matches!(
+        character,
+        '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9'
+    )
 }
 
-fn edit_size(
+fn edit_specific_size_update(
     mut char_input_events: EventReader<ReceivedCharacter>,
     mut edit_state: ResMut<State<EditState>>,
-    mut scan_code_input: ResMut<ScanCodeInput>,
-    mut edit_target: ResMut<OptionEditTarget>,
     q_active: Query<&NodeId, With<Active>>,
     mut tex_pro: ResMut<TextureProcessor>,
     mut q_instructions: Query<&mut Text, With<InstructionMarker>>,
+    mut started: Local<bool>,
 ) {
+    // This guard drops any input the first time the system is entered, so you do not get the
+    // input from the button that was pressed to start this sytem, in this sytem.
+    if !*started {
+        *started = true;
+        return;
+    }
+
     if let (Ok(mut instructions), Ok(node_id)) = (q_instructions.single_mut(), q_active.single()) {
         for event in char_input_events.iter() {
-            dbg!(&event.char);
             if is_number(&event.char) || event.char == 'x' {
                 instructions.sections[1].value.push(event.char);
             } else if event.char == '\u{8}' {
+                // Backspace
                 instructions.sections[1].value.pop();
             } else if event.char == '\r' {
-                // Try to parse the inputted value and put it where it should be
-                // Exit the mode
-                if let (Some(size), Some(node)) = (string_to_size(&instructions.sections[1].value), tex_pro.node_graph.node_with_id_mut(*node_id)) {
+                // Enter
+                if let (Some(size), Some(node)) = (
+                    string_to_size(&instructions.sections[1].value),
+                    tex_pro.node_graph.node_with_id_mut(*node_id),
+                ) {
                     node.resize_policy = ResizePolicy::SpecificSize(size);
                 } else {
                     warn!("Invalid size format, should be for instance 256x256");
                 }
                 edit_state.overwrite_replace(EditState::Outer).unwrap();
+                *started = false;
             }
         }
     }
 }
 
-fn string_to_size(input: &String) -> Option<TPSize> {
+fn string_to_size(input: &str) -> Option<TPSize> {
     let sizes: Vec<&str> = input.split('x').collect();
     if sizes.len() == 2 {
         if let (Ok(width), Ok(height)) = (sizes[0].parse(), sizes[1].parse()) {
@@ -202,16 +297,18 @@ fn edit(
             for scan_code in scan_codes {
                 if let Some(i) = scan_code.to_usize() {
                     match edit_target {
-                        EditTarget::ResizePolicy => {
-                            if let Some(resize_policy) = ResizePolicy::choose(i) {
-                                if let ResizePolicy::SpecificSize(_) = resize_policy {
-                                    edit_state.overwrite_replace(EditState::Size).unwrap();
-                                    return;
-                                } else {
-                                    node.resize_policy = resize_policy;
-                                }
+                        EditTarget::ResizePolicy => match ResizePolicy::choose(i) {
+                            Some(ResizePolicy::SpecificSize(_)) => {
+                                edit_state.overwrite_replace(EditState::Size).unwrap();
+                                return;
                             }
-                        }
+                            Some(ResizePolicy::SpecificSlot(_)) => {
+                                edit_state.overwrite_replace(EditState::Slot).unwrap();
+                                return;
+                            }
+                            Some(resize_policy) => node.resize_policy = resize_policy,
+                            None => (),
+                        },
                         EditTarget::ResizeFilter => {
                             if let Some(resize_filter) = ResizeFilter::choose(i) {
                                 node.resize_filter = resize_filter;
