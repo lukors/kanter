86259849195691c6890b61246f13d89e54261708
Lukas Orsvarn <lukas@orsvarn.com>
2021-05-05 16:20:42 +0200

    fix: Deselect tool (A) only works on its own
    No longer works when in edit mode for instance.
    
    Fixes #28

--- src/deselect_tool.rs
+++ src/deselect_tool.rs
@@ -3,7 +3,7 @@ use bevy::prelude::*;
 use crate::{
     instruction::ToolList,
     scan_code_input::{ScanCode, ScanCodeInput},
-    Selected, Stage,
+    Selected, Stage, ToolState,
 };
 
 pub(crate) struct DeselectToolPlugin;
@@ -12,7 +12,11 @@ impl Plugin for DeselectToolPlugin {
     fn build(&self, app: &mut AppBuilder) {
         app.add_startup_system(setup.system()).add_system_to_stage(
             CoreStage::Update,
-            deselect.system().label(Stage::Apply).after(Stage::Update),
+            deselect
+                .system()
+                .label(Stage::Apply)
+                .after(Stage::Update)
+                .with_run_criteria(State::on_update(ToolState::None)),
         );
     }
 }
