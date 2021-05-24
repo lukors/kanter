7714a589f9c8a6c4a2079813043e800369e69e04
Lukas Orsvarn <lukas@orsvarn.com>
2021-05-24 17:27:05 +0200

    refactor: Updated name of fn to match kanter_core

--- src/thumbnail.rs
+++ src/thumbnail.rs
@@ -156,7 +156,7 @@ fn try_get_output(tex_pro: &TextureProcessor) -> Result<Texture, TexProError> {
     Ok(Texture::new(
         Extent3d::new(slot_data.size.width as u32, slot_data.size.height as u32, 1),
         TextureDimension::D2,
-        slot_data.image.to_rgba(),
+        slot_data.image.to_u8(),
         TextureFormat::Rgba8Unorm,
     ))
 }
