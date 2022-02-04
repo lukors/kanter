278ec7669c334cf00400cc04f0c3585aabc21bf8
Lukas Orsvärn <lukas@orsvarn.com>
2022-02-04 20:44:01 +0100

    style: cargo fmt

--- src/drag_drop/mod.rs
+++ src/drag_drop/mod.rs
@@ -1,7 +1,7 @@
 pub mod edge;
 pub mod node;
 
-use crate::{AmbiguitySet, GrabToolType, ToolState, instruction::ToolList};
+use crate::{instruction::ToolList, AmbiguitySet, GrabToolType, ToolState};
 use bevy::prelude::*;
 
 use self::{
@@ -28,50 +28,54 @@ impl Plugin for WorkspaceDragDropPlugin {
     fn build(&self, app: &mut App) {
         app.add_startup_system(setup.system().in_ambiguity_set(AmbiguitySet))
             .add_system_set_to_stage(
-            CoreStage::Update,
-            SystemSet::new()
-                .with_system(
-                    grab_node_setup
-                        .system()
-                        .with_run_criteria(State::on_enter(ToolState::Grab(GrabToolType::Node)))
-                        .in_ambiguity_set(AmbiguitySet),
-                )
-                .with_system(
-                    grab_node_update
-                        .system()
-                        .chain(grab_node_update_edge)
-                        .with_run_criteria(State::on_update(ToolState::Grab(GrabToolType::Node)))
-                        .in_ambiguity_set(AmbiguitySet),
-                )
-                .with_system(
-                    grab_node_cleanup
-                        .system()
-                        .with_run_criteria(State::on_exit(ToolState::Grab(GrabToolType::Node)))
-                        .in_ambiguity_set(AmbiguitySet),
-                ),
-        )
-        .add_system_set_to_stage(
-            CoreStage::Update,
-            SystemSet::new()
-                .with_system(
-                    grab_tool_slot_setup
-                        .system()
-                        .with_run_criteria(State::on_enter(ToolState::Grab(GrabToolType::Slot)))
-                        .in_ambiguity_set(AmbiguitySet),
-                )
-                .with_system(
-                    grab_edge_update
-                        .system()
-                        .with_run_criteria(State::on_update(ToolState::Grab(GrabToolType::Slot)))
-                        .in_ambiguity_set(AmbiguitySet),
-                )
-                .with_system(
-                    grab_edge_cleanup
-                        .system()
-                        .with_run_criteria(State::on_exit(ToolState::Grab(GrabToolType::Slot)))
-                        .in_ambiguity_set(AmbiguitySet),
-                ),
-        );
+                CoreStage::Update,
+                SystemSet::new()
+                    .with_system(
+                        grab_node_setup
+                            .system()
+                            .with_run_criteria(State::on_enter(ToolState::Grab(GrabToolType::Node)))
+                            .in_ambiguity_set(AmbiguitySet),
+                    )
+                    .with_system(
+                        grab_node_update
+                            .system()
+                            .chain(grab_node_update_edge)
+                            .with_run_criteria(State::on_update(ToolState::Grab(
+                                GrabToolType::Node,
+                            )))
+                            .in_ambiguity_set(AmbiguitySet),
+                    )
+                    .with_system(
+                        grab_node_cleanup
+                            .system()
+                            .with_run_criteria(State::on_exit(ToolState::Grab(GrabToolType::Node)))
+                            .in_ambiguity_set(AmbiguitySet),
+                    ),
+            )
+            .add_system_set_to_stage(
+                CoreStage::Update,
+                SystemSet::new()
+                    .with_system(
+                        grab_tool_slot_setup
+                            .system()
+                            .with_run_criteria(State::on_enter(ToolState::Grab(GrabToolType::Slot)))
+                            .in_ambiguity_set(AmbiguitySet),
+                    )
+                    .with_system(
+                        grab_edge_update
+                            .system()
+                            .with_run_criteria(State::on_update(ToolState::Grab(
+                                GrabToolType::Slot,
+                            )))
+                            .in_ambiguity_set(AmbiguitySet),
+                    )
+                    .with_system(
+                        grab_edge_cleanup
+                            .system()
+                            .with_run_criteria(State::on_exit(ToolState::Grab(GrabToolType::Slot)))
+                            .in_ambiguity_set(AmbiguitySet),
+                    ),
+            );
     }
 }
 
